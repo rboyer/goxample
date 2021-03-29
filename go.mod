3ec05e7ad512f1c47a08445ffbecc4cd8b355f1a
R.B. Boyer <rb@hashicorp.com>
2021-03-29 10:32:49 -0500

    jazz hands

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/rboyer/goxample/v2
 go 1.15
 
 require (
+	github.com/rboyer/goxample v1.0.0 // indirect
 	go.etcd.io/bbolt v1.3.5
 	golang.org/x/sys v0.0.0-20210326220804-49726bf1d181 // indirect
 )
--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 
+	inner_v1 "github.com/rboyer/goxample/inner"
 	"github.com/rboyer/goxample/v2/inner"
 	_ "go.etcd.io/bbolt"
 )
 
 func main() {
 	fmt.Printf("HI FROM VEE %d\n", inner.Version())
+	fmt.Printf("BUT ALSO HI FROM VEE %d\n", inner_v1.Version())
 }
